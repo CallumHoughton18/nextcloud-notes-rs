5ced29d45dcf1d4954c423dceea9d834c4c0a38b
CallumHoughton18 <callum.houghton13@hotmail.co.uk>
2021-09-06 17:08:25 +0100

    add content to PostNoteCLIConfig. Add test for cliarguments

--- src/cliarguments.rs
+++ src/cliarguments.rs
@@ -3,7 +3,7 @@ use std::fmt::Debug;
 use std::fmt::Display;
 use std::vec::IntoIter;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Cmd {
     PostNote(PostNoteCLIConfig),
     ConfigPath(),
@@ -11,15 +11,16 @@ pub enum Cmd {
 }
 
 enum Operator {
-    PostNote,
+    PostNote(String),
     ConfigPath,
     Help
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub struct PostNoteCLIConfig {
     password: Option<String>,
     title: Option<String>,
+    content: String,
 }
 
 #[derive(Debug)]
@@ -89,7 +90,7 @@ pub fn parse_args(argv: Vec<String>) -> Result<Cmd, String> {
         match arg.flag_as_ref() {
             Arg::Plain("config-path") => operator = Operator::ConfigPath,
             Arg::Plain("help") => operator = Operator::Help,
-            Arg::Plain(_any_other_val) => operator = Operator::PostNote,
+            Arg::Plain(any_other_val) => operator = Operator::PostNote(String::from(any_other_val)),
             Arg::Short(flag, value) => {flag_map.insert(String::from(flag), value);}
             _ => return unexpected(arg),
         }
@@ -98,21 +99,69 @@ pub fn parse_args(argv: Vec<String>) -> Result<Cmd, String> {
     let cmd = match operator {
         Operator::ConfigPath => Cmd::ConfigPath(),
         Operator::Help => Cmd::Help(),
-        Operator::PostNote => Cmd::PostNote(parse_flags_to_post_note_cli_config(flag_map))
+        Operator::PostNote(content) => Cmd::PostNote(parse_flags_to_post_note_cli_config(flag_map, content))
     };
 
     println!("{:?}", cmd);
     Ok(cmd)
 }
 
-fn parse_flags_to_post_note_cli_config(mut flag_map: HashMap<String, String>) -> PostNoteCLIConfig {
+fn parse_flags_to_post_note_cli_config(mut flag_map: HashMap<String, String>, content: String) -> PostNoteCLIConfig {
     PostNoteCLIConfig {
         title: flag_map.remove("t"),
-        password: flag_map.remove("p")
+        password: flag_map.remove("p"),
+        content
     }
 
 }
 
 fn unexpected<T>(arg: Arg<String>) -> Result<T, String> {
     Err(format!("Unexpected argument {}. See 'tako --help'.", arg))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn parse_slice(args: &[&'static str]) -> Result<Cmd, String> {
+        let argv = args.iter().map(|s| String::from(*s)).collect();
+        parse_args(argv)
+    }
+    
+    #[test]
+    fn should_parse_help_command_correctly() {
+        let help_command = Ok(Cmd::Help());
+        assert_eq!(parse_slice(&["nxcloudnotes", "help"]), help_command);
+        assert_eq!(parse_slice(&["nxcloudnotes", "test", "bar" ,"help", "-ttestparams"]), help_command);
+        assert_eq!(parse_slice(&["nxcloudnotes", "-t\"testparams\"", "help"]), help_command)
+    }
+
+    #[test]
+    fn should_parse_config_path_command_correctly() {
+        let config_path_command = Ok(Cmd::ConfigPath());
+        assert_eq!(parse_slice(&["nxcloudnotes", "config-path"]), config_path_command);
+        assert_eq!(parse_slice(&["nxcloudnotes", "test", "bar" ,"config-path", "-ttestparams"]), config_path_command);
+        assert_eq!(parse_slice(&["nxcloudnotes", "-ttestparams", "config-path"]), config_path_command)
+    }
+
+    #[test]
+    fn should_parse_post_note_command_correctly() {
+        let post_note_command = Ok(Cmd::PostNote(PostNoteCLIConfig{
+            password: None,
+            title: None,
+            content: "note content here".to_string()
+        }));
+        assert_eq!(parse_slice(&["nxcloudnotes", "note content here"]), post_note_command);
+    }  
+
+    #[test]
+    fn should_parse_post_note_command_with_flags() {
+        let post_note_command = Ok(Cmd::PostNote(PostNoteCLIConfig{
+            password: Some("password".to_string()),
+            title: Some("title".to_string()),
+            content: String::from("note content here")
+        }));
+        assert_eq!(parse_slice(&["nxcloudnotes", "note content here", "-ttitle", "-ppassword"]), post_note_command);   
+        assert_eq!(parse_slice(&["nxcloudnotes", "-ttitle", "-ppassword", "note content here"]), post_note_command);   
+     }
 }
